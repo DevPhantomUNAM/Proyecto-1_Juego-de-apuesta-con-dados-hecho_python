c6a7d6285aeda28b6fdca82971d15b077b7cbcc2
Dev Phantom <Dev.Phantom.UNAM@gmail.com>
2020-11-24 15:00:52 -0600

    Se creo el proceso principal y se importo las librerias a utilizar

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+from tkinter import *
+from tkinter.filedialog import askopenfile 
+from tkinter import PhotoImage
+from PIL import Image,ImageTk
+import random
+
+
+if __name__ == "__main__":
+    main()
