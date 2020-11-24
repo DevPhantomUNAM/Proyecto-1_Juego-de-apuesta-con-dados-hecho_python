c50d1a23bd96baeea0dddce954a7768b25e934f7
Dev Phantom <Dev.Phantom.UNAM@gmail.com>
2020-11-24 15:01:43 -0600

    Se crea la interfaz con Tkinter

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from tkinter import PhotoImage
 from PIL import Image,ImageTk
 import random
 
+raiz = Tk()
+raiz.title('Juego de dados')
+raiz.geometry('1401x800') 
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
