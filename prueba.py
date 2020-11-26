4d922a506ac836bcb01e42a27c67946644ecb832
Dev Phantom <Dev.Phantom.UNAM@gmail.com>
2020-11-26 12:26:42 -0600

    Ordene las imagenes

---
+++ prueba.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.filedialog import askopenfile 
+from tkinter import PhotoImage
+from PIL import Image,ImageTk
+import random
+
+raiz = Tk()
+raiz.title('Juego de dados')
+raiz.geometry('1401x800') 
+
+raiz.mainloop()
